49ad422234fa4ed6393fc6f3ac338cf15ebed90f
AIright <eml.alexey@gmail.com>
2017-12-27 19:55:51 +0300

    Add files via upload
    Completed 3 week

--- src/headers/NamesAndSurenames.h
+++ src/headers/NamesAndSurenames.h
@@ -18,6 +18,13 @@
  * к концу данного года, но ещё и все предыдущие имена и фамилии в обратном хронологическом порядке.
  * Если текущие факты говорят о том, что человек два раза подряд изменил фамилию или имя на одно и то же,
  * второе изменение при формировании истории нужно игнорировать
+ * Дополните класс Person из задачи «Имена и фамилии — 2» конструктором, 
+ * позволяющим задать имя и фамилию человека при рождении, а также сам год рождения. 
+ * Класс не должен иметь конструктора по умолчанию.
+ * При получении на вход года, который меньше года рождения:
+ * методы GetFullName и GetFullNameWithHistory должны отдавать "No person";
+ * методы ChangeFirstName и ChangeLastName должны игнорировать запрос.
+ * Кроме того, необходимо объявить константными все методы, которые по сути ими являются.
  */
 
 #ifndef STUDY_C_NAMESANDSURENAMES1_H
@@ -34,7 +41,11 @@
 
 using namespace std;
 
-struct person {
+
+
+class Person {
+private:
+    // приватные поля
     // фиксируем изменение имени
     map<int, string> name;
     // список дат изменения имени для итерации
@@ -43,102 +54,84 @@ struct person {
     map<int, string> surename;
     // список дат изменения фамилии для итерации
     vector<int> surenames_years;
-};
 
-class Person {
 public:
+    Person(const string& init_name, const string& init_surename, const int& init_date) {
+        name[init_date] = init_name;
+        names_years.push_back(init_date);
+        surename[init_date] = init_surename;
+        surenames_years.push_back(init_date);
+    }
+
     void ChangeFirstName(int year, const string& first_name) {
         // добавить факт изменения имени на first_name в год year
-        person1.name[year] = first_name;
-        person1.names_years.push_back(year);
-        // сортируем вектор дат изменения имени
-        sort(person1.names_years.begin(), person1.names_years.end());
+        if (year > names_years[0]) {
+            name[year] = first_name;
+            names_years.push_back(year);
+            // сортируем вектор дат изменения имени
+            sort(names_years.begin(), names_years.end());
+        }
     }
 
     void ChangeLastName(int year, const string& last_name) {
         // добавить факт изменения фамилии на last_name в год year
-        person1.surename[year] = last_name;
-        person1.surenames_years.push_back(year);
-        // сортируем вектор дат изменения фамилии
-        sort(person1.surenames_years.begin(), person1.surenames_years.end());
-    }
-
-    string GetFullName(int year) {
-        // получить имя и фамилию по состоянию на конец года year
-        // год меньше минимального или дат нет вовсе
-        if ((person1.names_years.empty() || person1.names_years[0] > year)
-            && (person1.surenames_years.empty() || person1.surenames_years[0] > year)) {
-            return "Incognito";
-        // год больше максимального и изменения происходили (вектор дат не пустой)
-        } else if ((!person1.names_years.empty() && person1.names_years[person1.name.size() - 1] <= year)
-                   && (!person1.surenames_years.empty() && person1.surenames_years[person1.surename.size() - 1] <= year)){
-        // выводим изменения в последний год
-            return person1.name[person1.names_years[person1.name.size() - 1]] + " "
-                   + person1.surename[person1.surenames_years[person1.surename.size() - 1]];
-        // если дата изменения имени меньше минимальной или дат нет
-        } else if (person1.names_years.empty() || person1.names_years[0] > year) {
-            // то итерируем по фамилиям и выводим последнее изменение к дате year
-            int buf = Iterate(person1.surenames_years, year);
-            return person1.surename[buf] + " with unknown first name";
-        // если дата изменения фамилии меньше минимальной или дат нет
-        } else if (person1.surenames_years.empty() || person1.surenames_years[0] > year) {
-            // то итерируем по именам и выводим последнее изменение к дате year
-            int buf = Iterate(person1.names_years, year);
-            return person1.name[buf] + " with unknown last name";
-        } else {
-            // значит дата year внутри списков дат изменений имени и фамилии
-            // итерируем по датам изменений, находим ближайшие, выводим
-            int bufs = Iterate(person1.surenames_years, year);
-            int bufn = Iterate(person1.names_years, year);
-            return person1.name[bufn] + " " + person1.surename[bufs];
+        if (year > surenames_years[0]) {
+            surename[year] = last_name;
+            surenames_years.push_back(year);
+            // сортируем вектор дат изменения фамилии
+            sort(surenames_years.begin(), surenames_years.end());
         }
     }
 
-    string GetFullNameWithHistory(int year) {
-        // получить все имена и фамилии по состоянию на конец года year
-        // год меньше минимального или дат нет вовсе
-        if ((person1.names_years.empty() || person1.names_years[0] > year)
-            && (person1.surenames_years.empty() || person1.surenames_years[0] > year)) {
-            return "Incognito";
-        // год больше максимального и изменения происходили (вектор дат не пустой)
-        } else if ((!person1.names_years.empty() && person1.names_years[person1.name.size() - 1] <= year) &&
-                (!person1.surenames_years.empty() && person1.surenames_years[person1.surename.size() - 1] <= year)){
-            // выводим изменения в последний год
-            return person1.name[person1.names_years[person1.name.size() - 1]] +
-                    IteateHistory(person1.names_years, person1.names_years[person1.names_years.size() - 1], person1.name, 0) +
-                    person1.surename[person1.surenames_years[person1.surename.size() - 1]] +
-                    IteateHistory(person1.surenames_years, person1.surenames_years[person1.surenames_years.size() - 1], person1.surename, 1);
-            // если дата изменения имени меньше минимальной или дат нет
-        } else if (person1.names_years.empty() || person1.names_years[0] > year) {
-            // то итерируем по фамилиям и выводим последнее изменение к дате year
-            int buf = Iterate(person1.surenames_years, year);
-            return person1.surename[buf] +
-                    IteateHistory(person1.surenames_years, buf, person1.surename, 0) +
-                    "with unknown first name";
-            // если дата изменения фамилии меньше минимальной или дат нет
-        } else if (person1.surenames_years.empty() || person1.surenames_years[0] > year) {
-            // то итерируем по именам и выводим последнее изменение к дате year
-            int buf = Iterate(person1.names_years, year);
-            return person1.name[buf] +
-                    IteateHistory(person1.names_years, buf, person1.name, 0) +
-                    "with unknown last name";
+    string GetFullName(int& year) const {
+        // получить имя и фамилию по состоянию на конец года year
+        // год меньше минимального
+        if (year < names_years[0]) {
+            return "No person";
+        // год больше максимального
+        } else if ((names_years[name.size() - 1] <= year) && (surenames_years[surename.size() - 1] <= year)){
+        // выводим изменения в последний год
+            return name.at(names_years[name.size() - 1]) + " "
+                   + surename.at(surenames_years[surename.size() - 1]);
+        // если дата изменения имени меньше минимальной или дат нет
         } else {
             // значит дата year внутри списков дат изменений имени и фамилии
             // итерируем по датам изменений, находим ближайшие, выводим
-            int bufs = Iterate(person1.surenames_years, year);
-            int bufn = Iterate(person1.names_years, year);
-            return person1.name[bufn] +
-                    IteateHistory(person1.names_years, bufn, person1.name, 0) +
-                    person1.surename[bufs] +
-                    IteateHistory(person1.surenames_years, bufs, person1.surename, 1);
+            int bufs = Iterate(surenames_years, year);
+            int bufn = Iterate(names_years, year);
+            return name.at(bufn) + " " + surename.at(bufs);
+        }
+    }
+
+    string GetFullNameWithHistory(int& year) const {
+        // получить все имена и фамилии по состоянию на конец года year
+        // год меньше минимального
+        if (year < surenames_years[0]) {
+            return "No person";
+        // год больше максимального
+        } else if ((names_years[name.size() - 1] <= year) && (surenames_years[surename.size() - 1] <= year)){
+            // выводим изменения в последний год
+            return name.at(names_years[name.size() - 1]) +
+                    IteateHistory(names_years, names_years[names_years.size() - 1], name, 0) +
+                    surename.at(surenames_years[surename.size() - 1]) +
+                    IteateHistory(surenames_years, surenames_years[surenames_years.size() - 1], surename, 1);
+            // если дата изменения имени меньше минимальной или дат нет
+        } else {
+            // значит дата year внутри списков дат изменений имени и фамилии
+            // итерируем по датам изменений, находим ближайшие, выводим
+            int bufs = Iterate(surenames_years, year);
+            int bufn = Iterate(names_years, year);
+            return name.at(bufn) +
+                    IteateHistory(names_years, bufn, name, 0) +
+                    surename.at(bufs) +
+                    IteateHistory(surenames_years, bufs, surename, 1);
         }
     }
 
 private:
-    // приватные поля
-    person person1;
+    // приватные методы
     // функция итерации по векторам дат и вывода ближайшей к year даты
-    int Iterate(const vector<int>& years, const int& year) {
+    int Iterate(const vector<int>& years, const int& year) const {
         int buf = 0;
         for (const auto& i : years) {
             if (i > year) {
@@ -151,7 +144,7 @@ private:
 
     // функция итерации с историей
     string IteateHistory(const vector<int>& years, const int& year,
-            map<int, string>& names, const bool& last) {
+            const map<int, string>& names, const bool& last) const {
         string result = " ";
         vector<string> buf;
         // собираем в порядке изменения все уникальные фамилии
@@ -165,7 +158,7 @@ private:
             }
         }
         // если последнее изменение фамилии равно текущей, то удаляем
-        if (!buf.empty() && buf[buf.size() - 1] == names[year]) {
+        if (!buf.empty() && buf[buf.size() - 1] == names.at(year)) {
             buf.resize(buf.size() - 1);
         }
         // если фамилия менялась, то формируем строку с историей
