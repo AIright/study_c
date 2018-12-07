1ef4a50d23fd988e8f297a37634c688f6f7e883f
Aleksey Emelyanov <aleksey.emelyanov@gs-labs.tv>
2018-12-07 19:54:09 +0300

    completed task tracker

--- src/headers/TaskTracker.h
+++ src/headers/TaskTracker.h
@@ -5,117 +5,197 @@
 #ifndef STUDY_C_TASKTRACKER_H
 #define STUDY_C_TASKTRACKER_H
 
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <set>
+#include <vector>
+#include <string>
+#include <tuple>
+
+using namespace std;
+
 //Реализуйте класс TeamTasks, позволяющий хранить статистику по статусам задач команды разработчиков:
-//// Перечислимый тип для статуса задачи
-//enum class TaskStatus {
-//    NEW,          // новая
-//    IN_PROGRESS,  // в разработке
-//    TESTING,      // на тестировании
-//    DONE          // завершена
-//};
-//
-//// Объявляем тип-синоним для map<TaskStatus, int>,
-//// позволяющего хранить количество задач каждого статуса
-//using TasksInfo = map<TaskStatus, int>;
-//
-//class TeamTasks {
-//public:
-//    // Получить статистику по статусам задач конкретного разработчика
-//    const TasksInfo& GetPersonTasksInfo(const string& person) const;
-//
-//    // Добавить новую задачу (в статусе NEW) для конкретного разработчитка
-//    void AddNewTask(const string& person);
-//
-//    // Обновить статусы по данному количеству задач конкретного разработчика,
-//    // подробности см. ниже
-//    tuple<TasksInfo, TasksInfo> PerformPersonTasks(
-//            const string& person, int task_count);
-//};
-//
-//
-//Метод PerformPersonTasks должен реализовывать следующий алгоритм:
-//
-//Рассмотрим все не выполненные задачи разработчика person.
-//Упорядочим их по статусам: сначала все задачи в статусе NEW, затем все задачи в статусе IN_PROGRESS и, наконец, задачи в статусе TESTING.
-//Рассмотрим первые task_count задач и переведём каждую из них в следующий статус в соответствии с естественным порядком: NEW → IN_PROGRESS → TESTING → DONE.
-//Вернём кортеж из двух элементов: информацию о статусах обновившихся задач (включая те, которые оказались в статусе DONE) и информацию о статусах остальных не выполненных задач.
-//Гарантируется, что task_count является положительным числом. Если task_count превышает текущее количество невыполненных задач разработчика, достаточно считать, что task_count равен количеству невыполненных задач: дважды обновлять статус какой-либо задачи в этом случае не нужно.
-//
-//Кроме того, гарантируется, что метод GetPersonTasksInfo не будет вызван для разработчика, не имеющего задач.
-//
-//Пример работы метода PerformPersonTasks
-//Предположим, что у конкретного разработчика имеется 10 задач со следующими статусами:
-//
-//NEW — 3
-//IN_PROGRESS — 2
-//TESTING — 4
-//DONE — 1
-//Поступает команда PerformPersonTasks с параметром task_count = 4, что означает обновление статуса для 3 задач c NEW до IN_PROGRESS и для 1 задачи с IN_PROGRESS до TESTING. Таким образом, новые статусы задач будут следующими:
-//
-//IN_PROGRESS — 3 обновлённых, 1 старая
-//        TESTING — 1 обновлённая, 4 старых
-//        DONE — 1 старая
-//        В этом случае необходимо вернуть кортеж из 2 словарей:
-//
-//Обновлённые задачи: IN_PROGRESS — 3, TESTING — 1.
-//Не обновлённые задачи, исключая выполненные: IN_PROGRESS — 1, TESTING — 4.
-//Словари не должны содержать лишних элементов, то есть статусов, которым соответствует ноль задач.
-//
-//Примечание
-//        Обновление словаря одновременно с итерированием по нему может привести к непредсказуемым последствиям. При возникновении такой необходимости рекомендуется сначала в отдельном временном словаре собрать информацию об обновлениях, а затем применить их к основному словарю.
-//
+//Перечислимый тип для статуса задачи
+enum class TaskStatus {
+    NEW,          // новая
+    IN_PROGRESS,  // в разработке
+    TESTING,      // на тестировании
+    DONE          // завершена
+};
+// Объявляем тип-синоним для map<TaskStatus, int>,
+// позволяющего хранить количество задач каждого статуса
+using TasksInfo = map<TaskStatus, int>;
+
+class TeamTasks {
+private:
+    map<string, TasksInfo> tasks_database;
+public:
+    // Получить статистику по статусам задач конкретного разработчика
+    const TasksInfo& GetPersonTasksInfo(const string& person) const {
+        if (tasks_database.count(person)) {
+            return tasks_database.at(person);
+        }
+        return TasksInfo{};
+    };
+
+    // Добавить новую задачу (в статусе NEW) для конкретного разработчитка
+    void AddNewTask(const string& person) {
+        tasks_database[person][TaskStatus::NEW]++;
+    };
+
+    // Обновить статусы по данному количеству задач конкретного разработчика,
+    // подробности см. ниже
+    tuple<TasksInfo, TasksInfo> PerformPersonTasks(
+            const string& person, int task_count) {
+        // Prepare buffer for tasks count
+        TasksInfo person_tasks_count = tasks_database.at(person);
+        TasksInfo updated, not_updated;
+        int new_tasks = person_tasks_count[TaskStatus::NEW];
+        int in_progress_tasks = person_tasks_count[TaskStatus::IN_PROGRESS];
+        int testing_tasks = person_tasks_count[TaskStatus::TESTING];
+
+        for (int i = 0; i < task_count; i++) {
+            if (new_tasks != 0) {
+                new_tasks--;
+                updated[TaskStatus::IN_PROGRESS]++;
+            } else if (in_progress_tasks != 0) {
+                in_progress_tasks--;
+                updated[TaskStatus::TESTING]++;
+            } else if (testing_tasks != 0) {
+                testing_tasks--;
+                updated[TaskStatus::DONE]++;
+            }
+        }
+
+        if (new_tasks != 0) {
+            not_updated[TaskStatus::NEW] = new_tasks;
+        }
+        if (in_progress_tasks != 0) {
+            not_updated[TaskStatus::IN_PROGRESS] = in_progress_tasks;
+        }
+        if (testing_tasks != 0) {
+            not_updated[TaskStatus::TESTING] = testing_tasks;
+        }
+
+        // Update database (every incomplete task is a sum of updated and not updated
+        tasks_database[person][TaskStatus::NEW] = not_updated[TaskStatus::NEW];
+        tasks_database[person][TaskStatus::IN_PROGRESS] =
+                updated[TaskStatus::IN_PROGRESS] + not_updated[TaskStatus::IN_PROGRESS];
+        tasks_database[person][TaskStatus::TESTING] =
+                updated[TaskStatus::TESTING] + not_updated[TaskStatus::TESTING];
+        tasks_database[person][TaskStatus::DONE] += updated[TaskStatus::DONE];
+
+        // Erase zeros
+        for (auto i : tasks_database[person]) {
+            if (i.second == 0) {
+                tasks_database[person].erase(i.first);
+            }
+        }
+
+        return tie(updated, not_updated);
+    };
+};
+
+/*
+Метод PerformPersonTasks должен реализовывать следующий алгоритм:
+
+Рассмотрим все не выполненные задачи разработчика person.
+Упорядочим их по статусам: сначала все задачи в статусе NEW, затем все задачи в статусе IN_PROGRESS и,
+наконец, задачи в статусе TESTING.
+Рассмотрим первые task_count задач и переведём каждую из них в следующий статус в
+соответствии с естественным порядком: NEW → IN_PROGRESS → TESTING → DONE.
+Вернём кортеж из двух элементов: информацию о статусах обновившихся задач
+(включая те, которые оказались в статусе DONE) и информацию о статусах остальных не выполненных задач.
+Гарантируется, что task_count является положительным числом.
+Если task_count превышает текущее количество невыполненных задач разработчика, достаточно считать,
+что task_count равен количеству невыполненных задач:
+дважды обновлять статус какой-либо задачи в этом случае не нужно.
+
+Кроме того, гарантируется, что метод GetPersonTasksInfo не будет вызван для разработчика, не имеющего задач.
+
+Пример работы метода PerformPersonTasks
+Предположим, что у конкретного разработчика имеется 10 задач со следующими статусами:
+
+NEW — 3
+IN_PROGRESS — 2
+TESTING — 4
+DONE — 1
+Поступает команда PerformPersonTasks с параметром task_count = 4,
+что означает обновление статуса для 3 задач c NEW до IN_PROGRESS и для 1 задачи с IN_PROGRESS до TESTING.
+Таким образом, новые статусы задач будут следующими:
+
+IN_PROGRESS — 3 обновлённых, 1 старая
+TESTING — 1 обновлённая, 4 старых
+DONE — 1 старая
+В этом случае необходимо вернуть кортеж из 2 словарей:
+
+Обновлённые задачи: IN_PROGRESS — 3, TESTING — 1.
+Не обновлённые задачи, исключая выполненные: IN_PROGRESS — 1, TESTING — 4.
+Словари не должны содержать лишних элементов, то есть статусов, которым соответствует ноль задач.
+
+Примечание
+Обновление словаря одновременно с итерированием по нему может привести к непредсказуемым последствиям.
+При возникновении такой необходимости рекомендуется сначала в отдельном временном
+словаре собрать информацию об обновлениях, а затем применить их к основному словарю.
+*/
 //Пример кода
 //
-//// Принимаем словарь по значению, чтобы иметь возможность
-//// обращаться к отсутствующим ключам с помощью [] и получать 0,
-//// не меняя при этом исходный словарь
-//void PrintTasksInfo(TasksInfo tasks_info) {
-//    cout << tasks_info[TaskStatus::NEW] << " new tasks" <<
-//         ", " << tasks_info[TaskStatus::IN_PROGRESS] << " tasks in progress" <<
-//         ", " << tasks_info[TaskStatus::TESTING] << " tasks are being tested" <<
-//         ", " << tasks_info[TaskStatus::DONE] << " tasks are done" << endl;
-//}
-//
-//int main() {
-//    TeamTasks tasks;
-//    tasks.AddNewTask("Ilia");
-//    for (int i = 0; i < 3; ++i) {
-//        tasks.AddNewTask("Ivan");
-//    }
-//    cout << "Ilia's tasks: ";
-//    PrintTasksInfo(tasks.GetPersonTasksInfo("Ilia"));
-//    cout << "Ivan's tasks: ";
-//    PrintTasksInfo(tasks.GetPersonTasksInfo("Ivan"));
-//
-//    TasksInfo updated_tasks, untouched_tasks;
-//
-//    tie(updated_tasks, untouched_tasks) =
-//            tasks.PerformPersonTasks("Ivan", 2);
-//    cout << "Updated Ivan's tasks: ";
-//    PrintTasksInfo(updated_tasks);
-//    cout << "Untouched Ivan's tasks: ";
-//    PrintTasksInfo(untouched_tasks);
-//
-//    tie(updated_tasks, untouched_tasks) =
-//            tasks.PerformPersonTasks("Ivan", 2);
-//    cout << "Updated Ivan's tasks: ";
-//    PrintTasksInfo(updated_tasks);
-//    cout << "Untouched Ivan's tasks: ";
-//    PrintTasksInfo(untouched_tasks);
-//
-//    return 0;
-//}
+// Принимаем словарь по значению, чтобы иметь возможность
+// обращаться к отсутствующим ключам с помощью [] и получать 0,
+// не меняя при этом исходный словарь
+void PrintTasksInfo(TasksInfo tasks_info) {
+    cout << tasks_info[TaskStatus::NEW] << " new tasks" <<
+         ", " << tasks_info[TaskStatus::IN_PROGRESS] << " tasks in progress" <<
+         ", " << tasks_info[TaskStatus::TESTING] << " tasks are being tested" <<
+         ", " << tasks_info[TaskStatus::DONE] << " tasks are done" << endl;
+}
+
+int test() {
+    TeamTasks tasks;
+    tasks.AddNewTask("Ilia");
+    for (int i = 0; i < 3; ++i) {
+        tasks.AddNewTask("Ivan");
+    }
+    cout << "Ilia's tasks: ";
+    PrintTasksInfo(tasks.GetPersonTasksInfo("Ilia"));
+    cout << "Ivan's tasks: ";
+    PrintTasksInfo(tasks.GetPersonTasksInfo("Ivan"));
+
+    TasksInfo updated_tasks, untouched_tasks;
+
+    tie(updated_tasks, untouched_tasks) =
+            tasks.PerformPersonTasks("Ivan", 2);
+    cout << "Updated Ivan's tasks: ";
+    PrintTasksInfo(updated_tasks);
+    cout << "Untouched Ivan's tasks: ";
+    PrintTasksInfo(untouched_tasks);
+
+    tie(updated_tasks, untouched_tasks) =
+            tasks.PerformPersonTasks("Ivan", 2);
+    cout << "Updated Ivan's tasks: ";
+    PrintTasksInfo(updated_tasks);
+    cout << "Untouched Ivan's tasks: ";
+    PrintTasksInfo(untouched_tasks);
+
+    return 0;
+}
 //
 //Вывод
 //
-//        Ilia's tasks: 1 new tasks, 0 tasks in progress, 0 tasks are being tested, 0 tasks are done
+//Ilia's tasks: 1 new tasks, 0 tasks in progress, 0 tasks are being tested, 0 tasks are done
 //Ivan's tasks: 3 new tasks, 0 tasks in progress, 0 tasks are being tested, 0 tasks are done
 //Updated Ivan's tasks: 0 new tasks, 2 tasks in progress, 0 tasks are being tested, 0 tasks are done
 //Untouched Ivan's tasks: 1 new tasks, 0 tasks in progress, 0 tasks are being tested, 0 tasks are done
 //Updated Ivan's tasks: 0 new tasks, 1 tasks in progress, 1 tasks are being tested, 0 tasks are done
 //Untouched Ivan's tasks: 0 new tasks, 1 tasks in progress, 0 tasks are being tested, 0 tasks are done
 //Пояснение
-//        В этой задаче вам надо прислать на проверку файл с реализацией класса TeamTasks. Этот файл не должен содержать определения типов TaskStatus и TasksInfo. В противном случае вы получите ошибку компиляции.
-//
+//В этой задаче вам надо прислать на проверку файл с реализацией класса TeamTasks.
+// Этот файл не должен содержать определения типов TaskStatus и TasksInfo.
+// В противном случае вы получите ошибку компиляции.
 //Гарантируется, что типы TaskStatus и TasksInfo объявлены в точности так же, как в коде выше.
+
 #endif //STUDY_C_TASKTRACKER_H
